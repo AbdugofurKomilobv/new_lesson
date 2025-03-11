b355147f0974910b7a15336a1484ff9447ac5376
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-03-11 12:32:12 +0500

    baza bilan boglandi

--- common/views.py
+++ common/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import *
 
 
-def home_page(request):
-    return render(request=request,template_name='index.html')
+def home_page(request): 
+    news = News.objects.all()
+    db = {
+      "news":news,
+      "title": "News title"
+
+
+    }
+    return render(request=request,template_name='index.html',context=db)
